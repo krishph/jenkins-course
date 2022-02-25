66686f478d16ac7fa44ef55258e47e4b99c21e82
krishph <42297817+krishph@users.noreply.github.com>
2022-02-25 02:31:29 -0500

    change1

--- job-dsl/nodejs.groovy
+++ job-dsl/nodejs.groovy
@@ -1,4 +1,4 @@
-job('NodeJS example') {
+job('NodeJSexample') {
     scm {
         git('git://github.com/krishph/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
             node / gitConfigName('DSL User')
